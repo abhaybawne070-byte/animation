d53ef3a87f7f87034a5a4c2e3c60622d10b50e4a
abhay bawne <abhaybawne070@gmail.com>
2026-06-14 22:18:57 +0530

    feat: add initial box animation with start message logging

--- Day-02/src/main.js
+++ Day-02/src/main.js
@@ -1,7 +1,16 @@
 import gsap from 'gsap'
 import './style.css'
 
+gsap.to(".box",{
+    x:100,
+    direction:0.6,
+    delay : 1.8 , 
+    ease : "power2.inOut", 
 
+    onStart : ()=>{
+        console.log ("animation start !!")
+    }
+})
 
 // gsap.to(".box",{
 //     x : 500,
