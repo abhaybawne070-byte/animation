4d5123a7f688b087bcadc109cd2456ac0cf257a4
abhay bawne <abhaybawne070@gmail.com>
2026-06-15 11:03:18 +0530

    feat: update box animation sequence and fix box element references

--- Day-03/src/main.js
+++ Day-03/src/main.js
@@ -3,21 +3,28 @@ import './style.css'
 
 
 
-gsap.to(".box",{
+
+
+
+
+
+const tl = gsap.timeline();
+  
+tl.to(".box",{
   x : 500 , 
-  direction : 0.6 , 
-  delay : 1.3 ,
+  direction : 1.3, 
+  delay : 0.6,
   ease : "power4.out",
-
-})
-
-gsap.to(".box1",{
+}).to(".box1",{
   x:500,
   duration:1.3,
   ease : "power4.out",
-  delay:1.9 , 
-
+}).to(".box2",{
+  x : 500 ,
+  duration: 1.3 ,
+  ease : "power4.out"
+}).to(".box3",{
+  x:500,
+  duration : 1.3,
+  ease : "power4.out",
 })
-
-
-
