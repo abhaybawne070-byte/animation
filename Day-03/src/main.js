8537f5ca29753552a46d10eec6bcdc45554f3289
abhay bawne <abhaybawne070@gmail.com>
2026-06-15 10:03:29 +0530

    feat: remove heading animation and update box styles in HTML and CSS

--- Day-03/src/main.js
+++ Day-03/src/main.js
@@ -3,32 +3,21 @@ import './style.css'
 
 
 
-// gsap.to(".box",{
-//   x : 500 , 
-//   direction : 0.6 , 
-//   delay : 1.3 ,
-//   ease : "power4.out",
-//   repeat : -1,
-//   yoyo : true,
-//   stagger : {
-//      each  : 0.1,
-//      from : 'edges'
-
-//   }
-// })
-
-gsap.from("h1 span",{
-  yPercent : 100,
-  opacity: 0 ,
-  direction : 1.0 ,
-  
-  ease : "expo.out",
-  repeat : -1,
- 
-  stagger:{
-    each : 0.08,
-    from:'center'
-    
-  }
+gsap.to(".box",{
+  x : 500 , 
+  direction : 0.6 , 
+  delay : 1.3 ,
+  ease : "power4.out",
 
 })
+
+gsap.to(".box1",{
+  x:500,
+  duration:1.3,
+  ease : "power4.out",
+  delay:1.9 , 
+
+})
+
+
+
