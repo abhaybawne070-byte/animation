0ff6116b1ee60d3427966b9414a67250c8cec14a
abhay bawne <abhaybawne070@gmail.com>
2026-06-15 09:33:34 +0530

    feat: restructure HTML and CSS for heading animation, remove box elements

--- Day-03/src/main.js
+++ Day-03/src/main.js
@@ -3,15 +3,32 @@ import './style.css'
 
 
 
-gsap.to(".box",{
-  x : 500 , 
-  direction : 0.6 , 
-  delay : 1.3 ,
-  ease : "power4.out",
+// gsap.to(".box",{
+//   x : 500 , 
+//   direction : 0.6 , 
+//   delay : 1.3 ,
+//   ease : "power4.out",
+//   repeat : -1,
+//   yoyo : true,
+//   stagger : {
+//      each  : 0.1,
+//      from : 'edges'
+
+//   }
+// })
+
+gsap.from("h1 span",{
+  yPercent : 100,
+  opacity: 0 ,
+  direction : 1.0 ,
+  
+  ease : "expo.out",
   repeat : -1,
-  yoyo : true,
-  stagger : {
-     each  : 0.1,
-     from : 'edges'
+ 
+  stagger:{
+    each : 0.08,
+    from:'center'
+    
   }
+
 })
