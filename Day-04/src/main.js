046d930baa5cbccd05db12bc41e23695aeed698d
abhay bawne <abhaybawne070@gmail.com>
2026-06-15 17:23:52 +0530

    refactor: comment out unused timeline control code for clarity

--- Day-04/src/main.js
+++ Day-04/src/main.js
@@ -5,59 +5,74 @@ import './style.css'
 
 // timeline control 
 
-const play = document.querySelector(".play")
-const pause= document.querySelector(".pause")
-const restart= document.querySelector(".restart")
-const reverse= document.querySelector(".reverse")
-const seek= document.querySelector(".seek")
+// const play = document.querySelector(".play")
+// const pause= document.querySelector(".pause")
+// const restart= document.querySelector(".restart")
+// const reverse= document.querySelector(".reverse")
+// const seek= document.querySelector(".seek")
 
-const tl = gsap.timeline({paused:true})
+// const tl = gsap.timeline({paused:true})
 
-tl.to('.box',{
-  x:500,
-  duration:1.3,
-  delay:0.6 ,
-  ease : "power4.out"
-}).to('.box1',{
-  x:500,
-  duration:1.3,
-  ease: 'power4.out'
-}).to('.box2',{
-  x:500,
-  duration:1.3,
-  ease:'power4.out'
-}).to(".box2",{
-  x:500,
-  duration:1.3,
-  ease:'power4.out'
-})
-.addLabel("first")
-.to(".box3",{
-  x:500,
-  duration:1.3,
-  ease:'power4.out'
-})
-
-
- play.addEventListener("click",()=>{
-  tl.play()
- })
-
-  pause.addEventListener("click",()=>{
-  tl.pause()
- })
-
-  restart.addEventListener("click",()=>{
-  tl.restart()
- })
-
-  reverse.addEventListener("click",()=>{
-  tl.reverse()
- })
-
- seek.addEventListener("click",()=>{
-  tl.seek("first")
- })
+// tl.to('.box',{
+//   x:500,
+//   duration:1.3,
+//   delay:0.6 ,
+//   ease : "power4.out"
+// }).to('.box1',{
+//   x:500,
+//   duration:1.3,
+//   ease: 'power4.out'
+// }).to('.box2',{
+//   x:500,
+//   duration:1.3,
+//   ease:'power4.out'
+// }).to(".box2",{
+//   x:500,
+//   duration:1.3,
+//   ease:'power4.out'
+// })
+// .addLabel("first")
+// .to(".box3",{
+//   x:500,
+//   duration:1.3,
+//   ease:'power4.out'
+// })
 
 
+//  play.addEventListener("click",()=>{
+//   tl.play()
+//  })
 
+//   pause.addEventListener("click",()=>{
+//   tl.pause()
+//  })
+
+//   restart.addEventListener("click",()=>{
+//   tl.restart()
+//  })
+
+//   reverse.addEventListener("click",()=>{
+//   tl.reverse()
+//  })
+
+//  seek.addEventListener("click",()=>{
+//   tl.seek("first")
+//  })
+
+
+
+// this call nexted timeline
+
+// Loading timeline -
+const loadingTimeline = ()=>{
+  return gsap.timeline().to(Element,{})
+}
+
+// Navbar timeline -
+ const NevbarTimeline = ()=>{
+  return gsap.timeline().to(Element,{})
+ }
+
+ // Create a master timeline to control all the timeline
+ const master = gsap.timeline();
+ master.add(loadingTimeline,"-=0.4").add(NevbarTimeline)
