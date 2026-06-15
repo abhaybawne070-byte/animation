4f9382bdb508fa687eada9d02e47ad4117e3d985
abhay bawne <abhaybawne070@gmail.com>
2026-06-15 07:27:54 +0530

    feat: update box animation stagger configuration to use 'edges'

--- Day-03/src/main.js
+++ Day-03/src/main.js
@@ -8,8 +8,10 @@ gsap.to(".box",{
   direction : 0.6 , 
   delay : 1.3 ,
   ease : "power4.out",
+  repeat : -1,
+  yoyo : true,
   stagger : {
      each  : 0.1,
-     from : "center"
+     from : 'edges'
   }
 })
